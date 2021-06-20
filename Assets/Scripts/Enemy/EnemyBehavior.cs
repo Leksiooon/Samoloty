918879915e33daba57daf82e71c6f34fe1b2fe17
Leksiooon <sierocinskip.maciej@gmail.com>
2021-06-20 21:54:55 +0200

    Wybuch kompletny

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -7,22 +7,31 @@ public class EnemyBehavior : MonoBehaviour
     public float health = 50f;
     public GameObject effectsDestroy;
 
+    Vector3 center;
+    bool destroy = false;
+
+    private void Start()
+    {
+        center = GetComponent<Renderer>().bounds.center;
+    }
+
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if (health <= 0)
+        if (health <= 0 && !destroy)
         {
+            destroy = true;
             Die();
         }
     }
 
     void Die()
     {
-        GameObject effects = Instantiate(effectsDestroy, transform.position, Quaternion.LookRotation(Vector3.up));
+        GameObject effects = Instantiate(effectsDestroy, center, Quaternion.LookRotation(Vector3.up));
         effects.GetComponent<Effects>().Play();
-        
-        Destroy(gameObject,20f);
-        Destroy(effects,20f);
+
+        Destroy(gameObject, 20f);
+        Destroy(effects, 20f);
     }
 
 }
